9a37e362027366629e07a7903cc4dc516c42682f
tetsu5555 <tetete1118@gmail.com>
2019-03-31 11:16:11 +0900

    move javascript to script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// window.addEventListener("load", funciton() {
-//     alert("hello world")
-// })
+window.addEventListener("load", function () {
+    var els = document.querySelectorAll('#srcarea [class=item]');
+    for (var i = 0; i < els.length; i++) {
+        els[i].addEventListener('dragstart', function (evt) {
+            var elm = evt.target;
+            evt.dataTransfer.setData('Text', elm.id);
+            evt.stopPropagation();
+        }, false)
+    }
 
-console.log("hello")
+    var droparea = document.getElementById('droparea');
+    droparea.addEventListener("dragenter", function (evt) {
+        evt.preventDefault();
+    }, false);
+    droparea.addEventListener("dragover", function (evt) {
+        evt.preventDefault();
+    }, false);
+    droparea.addEventListener("drop", function (evt) {
+        var elm = evt.target;
+        var id = evt.dataTransfer.getData('Text');
+        var target = document.getElementById(id);
+        if (target) {
+            droparea.appendChild(target);
+        }
+        evt.preventDefault();
+    }, false);
 
-window.onload = function () {
-    alert("hello world");
-}
+})();
